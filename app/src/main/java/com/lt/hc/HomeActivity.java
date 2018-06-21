690ecaf416d516b255d5715e5d76c2193bd94769
sandeep.kamath <sandeep.kamath@robosoftin.com>
2018-06-21 16:42:20 +0530

    Integrated login

--- app/src/main/java/com/lt/hc/HomeActivity.java
+++ app/src/main/java/com/lt/hc/HomeActivity.java
@@ -1,11 +1,22 @@
 package com.lt.hc;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.lt.hc.game.Game;
 import com.lt.hc.game.GameConfig;
 import com.lt.hc.game.GameListener;
@@ -22,6 +33,7 @@ import butterknife.ButterKnife;
 public class HomeActivity extends AppCompatActivity {
 
 
+    private static final int RC_SIGN_IN = 101;
     @BindView(R.id.choice_view)
     RecyclerView choiceView;
 
@@ -36,7 +48,6 @@ public class HomeActivity extends AppCompatActivity {
         gameConfig.setWickets(1);
         gameConfig.setAIOpponent(true);
         final Game game = new Game(gameConfig, getGameListener());
-
 
 
         ArrayList<Choice> choices = new ArrayList<>();
@@ -56,17 +67,14 @@ public class HomeActivity extends AppCompatActivity {
         choiceView.setLayoutManager(new LinearLayoutManager(this));
         choiceView.setAdapter(choiceAdapter);
 
-
-
-
-
+        signInSilently();
     }
 
     private GameListener getGameListener() {
         return new GameListener() {
             @Override
-            public void playerScore(int score,int playerChoice,int opponentChoice) {
-                Log.d(Constants.TAG, "playerScore " + score +"\n playerChoice " +playerChoice+"\nopponentChoice "+opponentChoice);
+            public void playerScore(int score, int playerChoice, int opponentChoice) {
+                Log.d(Constants.TAG, "playerScore " + score + "\n playerChoice " + playerChoice + "\nopponentChoice " + opponentChoice);
             }
 
             @Override
@@ -95,8 +103,8 @@ public class HomeActivity extends AppCompatActivity {
             }
 
             @Override
-            public void opponentScore(int score,int playerChoice,int opponentChoice) {
-                Log.d(Constants.TAG, "opponentScore " + score +"\n playerChoice " +playerChoice+"\nopponentChoice "+opponentChoice);
+            public void opponentScore(int score, int playerChoice, int opponentChoice) {
+                Log.d(Constants.TAG, "opponentScore " + score + "\n playerChoice " + playerChoice + "\nopponentChoice " + opponentChoice);
             }
 
             @Override
@@ -120,4 +128,50 @@ public class HomeActivity extends AppCompatActivity {
             }
         };
     }
+
+
+    private void signInSilently() {
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+        signInClient.silentSignIn().addOnCompleteListener(this,
+                new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        if (task.isSuccessful()) {
+                            // The signed in account is stored in the task's result.
+                            GoogleSignInAccount signedInAccount = task.getResult();
+                        } else {
+                            // Player will need to sign-in explicitly using via UI
+                            startSignInIntent();
+                        }
+                    }
+                });
+    }
+
+    private void startSignInIntent() {
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // The signed in account is stored in the result.
+                GoogleSignInAccount signedInAccount = result.getSignInAccount();
+            } else {
+                String message = result.getStatus().getStatusMessage();
+                if (message == null || message.isEmpty()) {
+                    message = getString(R.string.signin_other_error);
+                }
+                new AlertDialog.Builder(this).setMessage(message)
+                        .setNeutralButton(android.R.string.ok, null).show();
+            }
+        }
+    }
+
 }
