7a9f48248aaa4a7994f45897afde0ac319fb9a09
Sandeep <sandeephkamath@gmail.com>
2018-06-28 01:15:12 +0530

    Add play games dependency

--- app/src/main/java/com/lovoctech/bluetoothhandcricket/game/dependency/GameModule.java
+++ app/src/main/java/com/lovoctech/bluetoothhandcricket/game/dependency/GameModule.java
@@ -4,6 +4,9 @@ import com.lovoctech.bluetoothhandcricket.game.Game;
 import com.lovoctech.bluetoothhandcricket.game.GameConfig;
 import com.lovoctech.bluetoothhandcricket.game.GameListener;
 import com.lovoctech.bluetoothhandcricket.game.Player;
+import com.lovoctech.bluetoothhandcricket.ui.model.Choice;
+
+import java.util.ArrayList;
 
 import dagger.Module;
 import dagger.Provides;
@@ -35,6 +38,17 @@ public class GameModule {
     @Provides
     Game providesGame(GameConfig gameConfig, Player player, Player opponent) {
         return new Game(gameConfig, gameListener, player, opponent);
+    }
+
+    @Provides
+    ArrayList<Choice> providesChoices() {
+        ArrayList<Choice> choices = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            Choice choice = new Choice();
+            choice.setValue(i);
+            choices.add(choice);
+        }
+        return choices;
     }
 
 }
